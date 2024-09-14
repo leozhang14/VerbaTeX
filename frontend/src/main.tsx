eccd919ccf50ff5473d98e1b77654f0ef42a0c0b
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-09-14 11:33:19 -0400

    use .env

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,15 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.tsx'
 import './index.css'
 
-// const domain = import.meta.env.REACT_APP_AUTH0_DOMAIN
-// const client_id = import.meta.env.REACT_APP_AUTH0_CLIENT_ID
-
+const domain = import.meta.env.VITE_AUTH0_DOMAIN
+const client_id =  import.meta.env.VITE_AUTH0_CLIENT_ID
 
 createRoot(document.getElementById('root')!).render(
+    
     <StrictMode>
         <Auth0Provider
-            domain="dev-mpn6iffi8psydlea.us.auth0.com"
-            clientId="4g4cYqXHL9Yd165rYtt8C1GVioPOIeJB"
+            domain={domain}
+            clientId={client_id}
             authorizationParams={{
                 redirect_uri: "http://localhost:5173"
             }}
