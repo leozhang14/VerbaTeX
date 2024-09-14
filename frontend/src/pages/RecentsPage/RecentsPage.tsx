a9eb4b18905b0a104856fd5348f23ddf50cae591
Zavista <84294353+Zavista@users.noreply.github.com>
2024-09-14 11:43:35 -0400

    set up router and pages

---
+++ frontend/src/pages/RecentsPage/RecentsPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecentsPage = () => {
+  return <div>RecentsPage</div>;
+};
+
+export default RecentsPage;
---
+++ frontend/src/pages/RecordPage/RecordPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+
+const RecordPage = () => {
+  return (
+    <div>
+      <Navbar title="Record new commands"></Navbar>
+      <div>Some Random Content</div>
+    </div>
+  );
+};
+
+export default RecordPage;
