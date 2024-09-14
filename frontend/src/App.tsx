76dc3128f45cbbb2cc5ed9a45bf0277b759429c3
Zavista <84294353+Zavista@users.noreply.github.com>
2024-09-14 03:33:00 -0400

    added space down logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import styles from "./styles/App.module.css";
 
 function App() {
+  const [isSpaceDown, setIsSpaceDown] = useState(false);
+  const audioContext = new AudioContext();
+
+  const handleSpaceDown = (event: KeyboardEvent) => {
+    if (event.code === "Space") {
+      event.preventDefault();
+      setIsSpaceDown(true);
+    }
+  };
+
+  const handleSpaceUp = (event: KeyboardEvent) => {
+    if (event.code === "Space") {
+      event.preventDefault();
+      setIsSpaceDown(false);
+    }
+  };
+
+  window.addEventListener("keydown", handleSpaceDown);
+  window.addEventListener("keyup", handleSpaceUp);
+
   return (
     <>
       <div className={styles.container}>
         <h1 className="mt-5 text-green-500 text-2xl">VerbaTeX</h1>
         <div className="mt-5 p-3 px-6 pt-2  text-black border-4 border-black rounded-full text-center">
-          Press space to record audio
+          {isSpaceDown ? "Recording..." : "Press SPACE to record audio"}
         </div>
+        <audio controls className="mt-5"></audio>
       </div>
     </>
   );
